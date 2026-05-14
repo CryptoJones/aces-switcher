ae7ef47ed1ea4648d9f78381e625f152fa0315b5
Aaron K. Clark <cryptojones@owasp.org>
2026-05-13 21:50:24 -0500

    fix: add IF EXIST guards before moves, add success message

--- aces-switcher.bat
+++ aces-switcher.bat
@@ -1,4 +1,19 @@
 @echo off
-move .\ACEventService.exe ACEventService.exe.tmp
-move .\ACEventService.exe.old ACEventService.exe
-move .\ACEventService.exe.tmp ACEventService.exe.old
+setlocal
+
+IF NOT EXIST "ACEventService.exe" (
+    echo ERROR: ACEventService.exe not found. Aborting.
+    exit /b 1
+)
+
+IF NOT EXIST "ACEventService.exe.old" (
+    echo ERROR: ACEventService.exe.old not found. Aborting.
+    exit /b 1
+)
+
+move /Y "ACEventService.exe"     "ACEventService.exe.tmp"
+move /Y "ACEventService.exe.old" "ACEventService.exe"
+move /Y "ACEventService.exe.tmp" "ACEventService.exe.old"
+
+echo Switched ACEventService binaries successfully.
+endlocal
